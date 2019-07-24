05ac4b8e3925158de586d6a944460e22b3b3066d
agnostics <Brandon.Padilla@broadridge.com>
2019-07-24 11:30:57 -0400

    Initial Commit

---
+++ public/electron.js
@@ -0,0 +1,33 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+const createWindow = () => {
+	mainWindow = new BrowserWindow({ width: 500, height: 600 });
+	mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+
+	if (isDev) {
+		mainWindow.webContents.openDevTools();
+	}
+
+	mainWindow.on("closed", () => (mainWindow = null));
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./global.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+		</div>
+	);
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
